325487aca611bbe8c03d9a14435b6ea8c66ca94f
marc7636 <marcus@familienskriver.dk>
2020-06-20 11:42:08 +0200

    Initial Initial Commit
    First man-made commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+	void OnDestroy()
+	{
+		
+	}
+}
